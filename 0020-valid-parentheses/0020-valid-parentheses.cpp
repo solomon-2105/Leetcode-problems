09ba466d46eb401f0dbc0823b9c2a3451935a7db
Solomon <solomon.prem.sinai@gmail.com>
2025-03-11 19:51:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return 0;
+                }
+                else if(s[i]==')'){
+                    if(st.top()=='('){
+                        st.pop();
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+                else if(s[i]=='}'){
+                    if(st.top()=='{'){
+                        st.pop();
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+                else if(s[i]==']'){
+                    if(st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+            }
+            }
+            return st.empty();
+    }
+};
