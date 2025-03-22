b86a273a5fdeda9027afcc51df50ea5c7fda2f0d
Solomon <solomon.prem.sinai@gmail.com>
2025-03-22 11:06:01 +0530

    Time: 20 ms (90.99%), Space: 127.9 MB (80.25%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,48 +1,36 @@
 #include <vector>
-#include <queue>
-#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, int& size, int& edgeCount) {
+        visited[node] = true;
+        size++;
+        edgeCount += adj[node].size();
+        
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, size, edgeCount);
+            }
+        }
+    }
+    
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<unordered_set<int>> adj(n);
+        vector<vector<int>> adj(n);
         vector<bool> visited(n, false);
         
         for (auto& edge : edges) {
-            adj[edge[0]].insert(edge[1]);
-            adj[edge[1]].insert(edge[0]);
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
         
         int count = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                queue<int> q;
-                unordered_set<int> component;
-                q.push(i);
-                visited[i] = true;
-                
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-                    component.insert(node);
-                    
-                    for (int neighbor : adj[node]) {
-                        if (!visited[neighbor]) {
-                            visited[neighbor] = true;
-                            q.push(neighbor);
-                        }
-                    }
-                }
-                
-                int size = component.size();
-                int edgeCount = 0;
-                
-                for (int node : component) {
-                    edgeCount += adj[node].size();
-                }
+                int size = 0, edgeCount = 0;
+                dfs(i, adj, visited, size, edgeCount);
                 
                 if (edgeCount / 2 == size * (size - 1) / 2) {
                     count++;
