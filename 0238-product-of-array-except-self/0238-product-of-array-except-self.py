7ac2b1e4f2c30644fac8c8e9db16927d1321fc4c
Solomon <solomon.prem.sinai@gmail.com>
2025-04-25 20:51:35 +0530

    Time: 31 ms (67.88%), Space: 20.3 MB (69.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,17 +6,12 @@ class Solution(object):
         """
         n = len(nums)
         result = [1] * n
-
-        # Calculate left products and store in result
         left_product = 1
         for i in range(n):
             result[i] = left_product
             left_product *= nums[i]
-
-        # Calculate right products and multiply with the left products in result
         right_product = 1
         for i in range(n - 1, -1, -1):
             result[i] *= right_product
             right_product *= nums[i]
-
         return result
