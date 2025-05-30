6232ea59eee201150fca341d8caf905a1499d5f4
Solomon <solomon.prem.sinai@gmail.com>
2025-05-30 19:43:54 +0530

    Time: 12 ms (84.63%), Space: 12.5 MB (13.02%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        i,j=0,0
+        res=[]
+        while i<len(word1) and j<len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i+=1
+            j+=1
+        res.extend(word1[i:])
+        res.extend(word2[j:])
+        return "".join(res)
