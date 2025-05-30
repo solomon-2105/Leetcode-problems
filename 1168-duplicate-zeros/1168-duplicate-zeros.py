e3017a1676e10c01b01a5275aaeeb0e084b6ef83
Solomon <solomon.prem.sinai@gmail.com>
2025-05-30 18:50:11 +0530

    Time: 6 ms (35.81%), Space: 13.2 MB (17.32%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -1,11 +1,24 @@
 class Solution:
-    def duplicateZeros(self, arr: List[int]) -> None:
-        n=len(arr)
-        i=0
-        while i<n:
-            if arr[i]==0:
-                arr.pop()
-                arr.insert(i,0)
-                i+=2
-            else:
-                i+=1
+    def duplicateZeros(self, arr):
+        flag = 0
+        zoffset = 10
+        nzoffset = 100
+        n = len(arr)
+        zp = nzp = 0
+
+        while zp < n:
+            val = arr[nzp]
+            if flag == 1:
+                val = (arr[nzp] % nzoffset) // zoffset
+            if val == 0:
+                arr[zp] = (arr[nzp] % nzoffset) * nzoffset + arr[zp] * zoffset
+                flag = 1
+                zp += 1
+            if flag == 1 and zp < n:
+                arr[zp] = val * nzoffset + arr[zp] * zoffset
+            zp += 1
+            nzp += 1
+
+        for i in range(n):
+            if arr[i] >= zoffset:
+                arr[i] = (arr[i] // nzoffset) % zoffset
