833a901c5fede8b4b247196fa655cf33c2e50d96
Solomon <solomon.prem.sinai@gmail.com>
2025-06-01 07:07:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (31.09%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # n=len(nums)
+        # prefix=[0]*n
+        # prefix[0]=nums[0]
+        # for i in range(1,n):
+        #     prefix[i]=prefix[i-1]+nums[i]
+        # return prefix
         n=len(nums)
-        prefix=[0]*n
-        prefix[0]=nums[0]
+        # prefix=[0]*n
+        # prefix[0]=nums[0]
         for i in range(1,n):
-            prefix[i]=prefix[i-1]+nums[i]
-        return prefix
+            nums[i]=nums[i-1]+nums[i]
+        return nums
