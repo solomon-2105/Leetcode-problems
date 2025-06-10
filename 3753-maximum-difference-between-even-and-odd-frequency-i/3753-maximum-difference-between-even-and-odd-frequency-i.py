146b6aa37bd2e19416a716461494bd14ad7280af
Solomon <solomon.prem.sinai@gmail.com>
2025-06-10 17:51:01 +0530

    Time: 4 ms (32.65%), Space: 17.9 MB (47.42%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,12 +1,22 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        brr=[0]*26
+        # brr=[0]*26
         mini,maxi=len(s),0
-        for  i in s:
-            brr[ord(i)-ord('a')]+=1
-        for i in range(26):
-            if (brr[i]%2)!=0:
-                maxi=max(maxi,brr[i])
-            if (brr[i]%2)==0 and brr[i]>0:
-                mini=min(mini,brr[i])
+        # for  i in s:
+        #     brr[ord(i)-ord('a')]+=1
+        # for i in range(26):
+        #     if (brr[i]%2)!=0:
+        #         maxi=max(maxi,brr[i])
+        #     if (brr[i]%2)==0 and brr[i]>0:
+        #         mini=min(mini,brr[i])
+        # return maxi-mini
+
+        brr={}
+        for i in s:
+            brr[i]=brr.get(i,0)+1
+        for i,j in brr.items():
+            if (j%2)!=0:
+                maxi=max(maxi,j)
+            if (j%2)==0 and j>0:
+                mini=min(mini,j)
         return maxi-mini
