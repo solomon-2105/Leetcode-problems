2066ed72665b9c94ee5dfafc7253747bce21b615
Solomon <solomon.prem.sinai@gmail.com>
2025-07-22 17:06:40 +0530

    Time: 12 ms (6.15%), Space: 18.3 MB (78.88%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        def brr(ass,x,y):
+            a=len(ass)
+            b=len(ass[0])
+            for j in range(b):
+                if ass[x][j]!=0:  ass[x][j]="M"
+            for i in range(a):
+                if ass[i][y]!=0: ass[i][y]="M"
+        m=len(matrix)
+        n=len(matrix[0])
+        count=0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]==0:
+                    brr(matrix,i,j)
+                    count+=1
+                
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]=="M":
+                    matrix[i][j]=0
+
