0a4efa29843ea6a3749352cbb051531535bd61e1
Solomon <solomon.prem.sinai@gmail.com>
2025-07-23 03:53:10 +0530

    Time: 0 ms (100%), Space: 17.7 MB (66.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,12 +4,8 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n=len(matrix)
-        for i in range(n):
+        for i in range(n-1):
             for j in range(i+1,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for i in range(n):
-            for j in range(n//2):
-                matrix[i][j],matrix[i][n-j-1]=matrix[i][n-j-1],matrix[i][j]
-
-        
-        
+            matrix[i].reverse()
