18e0bd8db538caaa254b247a529a305f32df8187
Solomon <solomon.prem.sinai@gmail.com>
2025-07-23 09:29:19 +0530

    Time: 3 ms (71.08%), Space: 18.5 MB (54.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        #bruteforce
+
         # def brr(ass,x,y):
         #     a=len(ass)
         #     b=len(ass[0])
@@ -12,29 +14,51 @@ class Solution:
         #         if ass[i][y]!=0: ass[i][y]="M"
         # m=len(matrix)
         # n=len(matrix[0])
-        # count=0
         # for i in range(m):
         #     for j in range(n):
         #         if matrix[i][j]==0:
         #             brr(matrix,i,j)
-        #             count+=1
                 
         # for i in range(m):
         #     for j in range(n):
         #         if matrix[i][j]=="M":
         #             matrix[i][j]=0
+        
+        #better
+        # m=len(matrix)
+        # n=len(matrix[0]) 
+        # a=[0]*m
+        # b=[0]*n
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j]==0:
+        #             a[i]=b[j]=1       
+        # for i in range(m):
+        #     for j in range(n):
+        #         if b[j]==1 or a[i]==1:
+        #             matrix[i][j]=0
+
+        #optimal anta
         m=len(matrix)
-        n=len(matrix[0]) 
-        a=[0]*m
-        b=[0]*n
+        n=len(matrix[0])
+        col0=1
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]==0:
-                    a[i]=b[j]=1       
-        for i in range(m):
+                    matrix[i][0]=0
+                    if j!=0: matrix[0][j]=0
+                    else: col0=0
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j]!=0:
+                    if matrix[i][0]==0 or matrix[0][j]==0:
+                        matrix[i][j]=0
+        if matrix[0][0]==0:
             for j in range(n):
-                if b[j]==1 or a[i]==1:
-                    matrix[i][j]=0
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(m):
+                matrix[i][0]=0
 
 
 
