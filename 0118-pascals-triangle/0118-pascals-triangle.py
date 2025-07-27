48a0a6d75098b0658267ae21ab2847a2dc24b49f
Solomon <solomon.prem.sinai@gmail.com>
2025-07-27 18:27:11 +0530

    Time: 0 ms (100%), Space: 17.6 MB (87.16%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,22 +1,9 @@
 class Solution:
-    def generate(self, n: int) -> List[List[int]]:
-        #combinatorics
-
-        # grr=[]
-        # for i in range(n):
-        #     res=1
-        #     brr=[1]
-        #     for j in range(1,i+1):
-        #         res*=(i-j+1)
-        #         res//=j
-        #         brr.append(res)
-        #     grr.append(brr)
-        # return grr
-
-
-        #Dynamic programming
-        a=[[1]*(i+1) for i in range(n)]
-        for i in range(2,n):
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for i in range(numRows):
+            row=[1]*(i+1)
             for j in range(1,i):
-                a[i][j]=a[i-1][j-1]+a[i-1][j]
-        return a
+                row[j]=triangle[i-1][j-1]+triangle[i-1][j]
+            triangle.append(row)
+        return triangle
