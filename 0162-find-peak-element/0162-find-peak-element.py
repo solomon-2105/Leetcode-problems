bfdfc21915784fe344360032a8ced4d4c23cf8f7
Solomon <solomon.prem.sinai@gmail.com>
2025-08-05 16:38:39 +0530

    Time: 0 ms (100%), Space: 17.8 MB (88.51%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -5,7 +5,10 @@ class Solution:
         low,high=0,c-1
         while low<=high:
             mid=low+(high-low)//2
-            if (mid==0 and c==1) or (mid==0 and a[mid]>a[mid+1]) or (mid==c-1 and a[mid]>a[mid-1]) or (mid>0 and mid<c-1 and a[mid]>a[mid-1] and a[mid]>a[mid+1]):
+            if (mid==0 and c==1) or \
+   (mid==0 and a[mid]>a[mid+1]) or \
+   (mid==c-1 and a[mid]>a[mid-1]) or\
+   (mid>0 and mid<c-1 and a[mid]>a[mid-1] and a[mid]>a[mid+1]):
                 return mid
             elif mid<c-1 and a[mid]<a[mid+1]:
                 low=mid+1
