930f4210e4b733e78b8619a1b3c5e077f8d80da7
Solomon <solomon.prem.sinai@gmail.com>
2025-08-08 18:39:04 +0530

    Time: 147 ms (85.64%), Space: 18.9 MB (47.49%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self,a:List[int],h:int)->int:
+        def brr(a:List[int],i:int)->int:
+            s=0
+            for j in a:
+                s+=(j+i-1)//i
+            return s
+        grr=0
+        for val in a:
+            grr=max(grr,val)
+        low,high=1,grr
+        while low<=high:
+            mid=low+(high-low)//2
+            req=brr(a,mid)
+            if req<=h:
+                high=mid-1
+            else:
+                low=mid+1
+        return low
