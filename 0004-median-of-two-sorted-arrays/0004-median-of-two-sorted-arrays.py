a3874d44be76f88cee4d17e33d85d4b67cd4327b
Solomon <solomon.prem.sinai@gmail.com>
2025-08-14 17:35:39 +0530

    Time: 4 ms (33.51%), Space: 18.2 MB (19.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,56 +1,65 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        # bruteforce TC- O(m+n)
-        # i=j=0
-        # merged=[]
-        # while i<len(nums1) and j<len(nums2):
-        #     if nums1[i]<=nums2[j]:
-        #         merged.append(nums1[i])
-        #         i+=1
-        #     else:
-        #         merged.append(nums2[j])
-        #         j+=1
-        # while i<len(nums1):
-        #     merged.append(nums1[i])
-        #     i+=1
-        # while j<len(nums2):
-        #     merged.append(nums2[j])
-        #     j+=1
-        # n=len(merged)
-        # if n%2==1:
-        #     return merged[n//2]
-        # else:
-        #     return (merged[n//2-1]+merged[n//2])/2.0
-
-        if len(nums1)>len(nums2):
-            nums1,nums2=nums2,nums1
-        m,n=len(nums1),len(nums2)
-        total=m+n
-        half=(total+1)//2 # Half size when merging both arrays
-        left,right=0,m  # Binary search boundaries on nums1
-        while left<=right:
-            i=(left+right)//2  # Partition in nums1
-            j=half-i         # Partition in nums2
-            # Handle edges: If partition is at start or end
-            nums1_left=float('-inf') if i == 0 else nums1[i-1]
-            nums1_right=float('inf') if i == m else nums1[i]
-            nums2_left=float('-inf') if j == 0 else nums2[j-1]
-            nums2_right=float('inf') if j == n else nums2[j]
-            # Check if the partition is correct
-            if nums1_left<=nums2_right and nums2_left<=nums1_right:
-                # If total number of elements is odd
-                if total%2==1:
-                    return float(max(nums1_left,nums2_left))
-                else:
-                    return (max(nums1_left,nums2_left)+min(nums1_right,nums2_right))/2.0
-            elif nums1_left>nums2_right:
-                # Move left in nums1
-                right=i-1
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        def find_median(a,b):
+            c,d=len(a),len(b)
+            total=c+d
+            i=j=brr=0
+            if total%2==0:
+                mid1=total//2-1
+                mid2=total//2
+                val1=val2=None
+                while i<c and j<d:
+                    if a[i]<=b[j]:
+                        if brr==mid1:
+                            val1=a[i]
+                        if brr==mid2:
+                            val2=a[i]
+                            return (val1+val2)/2
+                        i+=1
+                    else:
+                        if brr==mid1:
+                            val1=b[j]
+                        if brr==mid2:
+                            val2=b[j]
+                            return (val1+val2)/2
+                        j+=1
+                    brr+=1
+                while i<c:
+                    if brr==mid1:
+                        val1=a[i]
+                    if brr==mid2:
+                        val2=a[i]
+                        return (val1+val2)/2
+                    i+=1
+                    brr+=1
+                while j<d:
+                    if brr==mid1:
+                        val1=b[j]
+                    if brr==mid2:
+                        val2=b[j]
+                        return (val1+val2)/2
+                    j+=1
+                    brr+=1
             else:
-                # Move right in nums1
-                left=i+1
+                mid=total//2
+                while i<c and j<d:
+                    if a[i]<=b[j]:
+                        if brr==mid:
+                            return a[i]
+                        i+=1
+                    else:
+                        if brr==mid:
+                            return b[j]
+                        j+=1
+                    brr+=1
+                while i<c:
+                    if brr==mid:
+                        return a[i]
+                    i+=1
+                    brr+=1
+                while j<d:
+                    if brr==mid:
+                        return b[j]
+                    j+=1
+                    brr+=1
+        return find_median(nums1,nums2)
