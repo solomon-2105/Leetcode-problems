e242f47f63aae8104ee9b9d798fd58854fcdfbad
Solomon <solomon.prem.sinai@gmail.com>
2025-08-17 00:33:08 +0530

    Time: 4 ms (33.55%), Space: 18.1 MB (58.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,65 +1,25 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        def find_median(a,b):
-            c,d=len(a),len(b)
-            total=c+d
-            i=j=brr=0
-            if total%2==0:
-                mid1=total//2-1
-                mid2=total//2
-                val1=val2=None
-                while i<c and j<d:
-                    if a[i]<=b[j]:
-                        if brr==mid1:
-                            val1=a[i]
-                        if brr==mid2:
-                            val2=a[i]
-                            return (val1+val2)/2
-                        i+=1
-                    else:
-                        if brr==mid1:
-                            val1=b[j]
-                        if brr==mid2:
-                            val2=b[j]
-                            return (val1+val2)/2
-                        j+=1
-                    brr+=1
-                while i<c:
-                    if brr==mid1:
-                        val1=a[i]
-                    if brr==mid2:
-                        val2=a[i]
-                        return (val1+val2)/2
-                    i+=1
-                    brr+=1
-                while j<d:
-                    if brr==mid1:
-                        val1=b[j]
-                    if brr==mid2:
-                        val2=b[j]
-                        return (val1+val2)/2
-                    j+=1
-                    brr+=1
-            else:
-                mid=total//2
-                while i<c and j<d:
-                    if a[i]<=b[j]:
-                        if brr==mid:
-                            return a[i]
-                        i+=1
-                    else:
-                        if brr==mid:
-                            return b[j]
-                        j+=1
-                    brr+=1
-                while i<c:
-                    if brr==mid:
-                        return a[i]
-                    i+=1
-                    brr+=1
-                while j<d:
-                    if brr==mid:
-                        return b[j]
-                    j+=1
-                    brr+=1
-        return find_median(nums1,nums2)
+    def findMedianSortedArrays(self, a: List[int], b: List[int]) -> float:
+        c,d=len(a),len(b)
+        if c>d:
+            a,b=b,a
+            c,d=d,c
+        low,high=0,c
+        while low<=high:
+            mid1=low+(high-low)//2
+            mid2=(c+d+1)//2-mid1
+            
+            l1=a[mid1-1] if mid1>0 else float('-inf')
+            r1=a[mid1] if mid1<c else float('inf')
+            l2=b[mid2-1] if mid2>0 else float('-inf')
+            r2=b[mid2] if mid2<d else float('inf')
+
+            if l1<=r2 and l2<=r1:
+                if (c+d)%2==0:
+                    return (max(l1,l2)+min(r1,r2))/2
+                else:
+                    return max(l1,l2)
+            elif l1>r2:
+                high=mid1-1
+            elif l2>r1:
+                low=mid1+1
