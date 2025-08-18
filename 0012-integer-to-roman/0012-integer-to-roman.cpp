83363c3afefb39720704f6d0f38ba59b5e6e645f
Solomon <solomon.prem.sinai@gmail.com>
2025-08-18 11:48:56 +0530

    Time: 0 ms (100%), Space: 12.7 MB (20.5%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int,string>> brr={
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
+        };
+        string res="";
+        for(auto &c: brr){
+            while(num>=c.first){
+                res+=c.second;
+                num-=c.first;
+            }
+        }
+        return res;
+    }
+};
