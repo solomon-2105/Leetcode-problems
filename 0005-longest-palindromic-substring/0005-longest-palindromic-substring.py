65cd85be2e957fa1205e1d9f29479aa8090481f9
Solomon <solomon.prem.sinai@gmail.com>
2025-08-18 13:37:08 +0530

    Time: 281 ms (62.68%), Space: 17.8 MB (59.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,20 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        start = 0
-        max_len = 1
-
-        def expand(l: int, r: int):
-            nonlocal start, max_len
-            while l >= 0 and r < n and s[l] == s[r]:
-                if (r - l + 1) > max_len:
-                    start = l
-                    max_len = r - l + 1
-                l -= 1
-                r += 1
-
-        for i in range(n):
-            expand(i, i)       # Odd-length palindrome
-            expand(i, i + 1)   # Even-length palindrome
-
-        return s[start:start + max_len]
+    #     maxlen=float('-inf')
+    #     sp=0
+    #     n=len(s)
+    #     for i in range(n):
+    #         for j in range(i,n):
+    #             if self.palindrome(s,i,j):
+    #                 if maxlen<(j-i+1):
+    #                     maxlen=j-i+1
+    #                     sp=i
+    #     return s[sp:sp+maxlen]
+    
+    # def palindrome(self,s:str,i:int,j:int)->bool:
+    #     l,h=i,j
+    #     while l<=h:
+    #         if s[l]!=s[h]: return False
+    #         else:
+    #             l+=1
+    #             h-=1
+    #     return True
+        n=len(s)
+        maxlen=float('-inf')
+        sp=0
+        for i in range(len(s)):
+            l,r=self.brr(s,i,i)
+            if r-l+1>maxlen:
+                maxlen=r-l+1
+                sp=l
+            l,r=self.brr(s,i,i+1)
+            if r-l+1>maxlen:
+                maxlen=r-l+1
+                sp=l
+        return s[sp:sp+maxlen]
+    
+    def brr(self,s,l,r):
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            l-=1
+            r+=1
+        return l+1,r-1
