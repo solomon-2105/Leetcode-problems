1cc54113061502b98472497ffa7bf777ea287c13
Solomon <solomon.prem.sinai@gmail.com>
2025-08-19 12:33:37 +0530

    Time: 10 ms (93.02%), Space: 12.5 MB (50.13%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n==0: return 0
-        if n==1: return 1
-        a=[0,1]
-        for i in range(2,n+1):
-            a.append(a[i-1]+a[i-2])
-        return a[n]
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        else:
+            a=0
+            b=1
+            for i in range(2,n+1):
+                c=a+b
+                a=b
+                b=c
+            return b
+        
+        
