d02ed0cdfeea04cab68ef9d12942344fb55a0b27
Solomon <solomon.prem.sinai@gmail.com>
2025-08-30 21:29:11 +0530

    Time: 2 ms (45.55%), Space: 18 MB (29.77%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -10,10 +10,10 @@ class Solution:
             i+=1
         elif s[i]=="+":
             i+=1
-        num=0
-        while i<len(s) and s[i].isdigit():
-            num=num*10+(ord(s[i])-ord('0'))
-            i+=1
+        def rec(i,num):
+            if i==len(s) or not s[i].isdigit(): return num
+            return rec(i+1,num*10+(ord(s[i])-ord('0')))
+        num=rec(i,0)
         if brr: num*=-1
         num=max(-2**31,min(2**31-1,num))
         return num
