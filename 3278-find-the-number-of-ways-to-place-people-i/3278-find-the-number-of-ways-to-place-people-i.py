53f65795e5014facda700f79067e65d1bb694bea
Solomon <solomon.prem.sinai@gmail.com>
2025-09-05 12:39:53 +0530

    Time: 47 ms (47.5%), Space: 18 MB (20.1%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.py
@@ -1,17 +1,38 @@
+# class Solution:
+#     def numberOfPairs(self, p: List[List[int]]) -> int:
+#         #O(n^3) tc
+#         count=0
+#         for i in range(len(p)):
+#             c,d=p[i]
+#             for j in range(len(p)):
+#                 if j!=i:
+#                     a,b=p[j]
+#                     if c<=a and d>=b:
+#                         brr=True
+#                         for k in range(len(p)):
+#                             if k!=i and k!=j:
+#                                 q,r=p[k]
+#                                 if c<=q<=a and b<=r<=d:
+#                                     brr=False
+#                         if brr:count+=1
+#         return count
+
 class Solution:
     def numberOfPairs(self, p: List[List[int]]) -> int:
+        p.sort(key=lambda x: (x[0], -x[1]))  # sort by x asc, y desc
+        n=len(p)
         count=0
-        for i in range(len(p)):
+        for i in range(n):
             c,d=p[i]
-            for j in range(len(p)):
-                if j!=i:
-                    a,b=p[j]
-                    if c<=a and d>=b:
-                        brr=True
-                        for k in range(len(p)):
-                            if k!=i and k!=j:
-                                q,r=p[k]
-                                if c<=q<=a and b<=r<=d:
-                                    brr=False
-                        if brr:count+=1
+            for j in range(i+1,n):
+                a,b=p[j]
+                if d>=b:  # A must be upper-left of B
+                    valid=True
+                    for k in range(i+1,j):  # only check points in between
+                        q,r=p[k]
+                        if c<=q<=a and b<=r<=d:
+                            valid=False
+                            break
+                    if valid: count+=1
         return count
+
