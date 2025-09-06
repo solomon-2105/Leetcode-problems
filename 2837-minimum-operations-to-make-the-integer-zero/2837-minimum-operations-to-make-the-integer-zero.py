9b1b5e8c25252c4fbe81eb79ee35ee142fb5ba35
Solomon <solomon.prem.sinai@gmail.com>
2025-09-06 18:19:38 +0530

    Time: 4 ms (2.41%), Space: 17.7 MB (78.31%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.py
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.py
@@ -2,9 +2,10 @@ class Solution:
     def makeTheIntegerZero(self, a: int, b: int) -> int:
         for i in range(1,61):
             brr=a-i*b
-            if brr<i:
-                continue
+            # if brr<i:
+            #     continue
             p=bin(brr).count("1")
+            # if p<=i<=brr:
             if p<=i<=brr:
                 return i
         return -1
