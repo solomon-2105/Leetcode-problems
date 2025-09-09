975d4f9bc65fb61fc3b20d9e964fa21acf9d8c69
Solomon <solomon.prem.sinai@gmail.com>
2025-09-09 19:20:26 +0530

    Time: 55 ms (57.56%), Space: 49 MB (38.05%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.py
@@ -5,20 +5,30 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     if head is None or head.next is None:
+    #         return None
+    #     size=self.length(head)
+    #     brr=size//2
+    #     cur=head
+    #     for _ in range(brr-1):
+    #         cur=cur.next
+    #     cur.next=cur.next.next
+    #     return head
+
+    # def length(self,head):
+    #     cur=head
+    #     count=0
+    #     while cur is not None:
+    #         count+=1
+    #         cur=cur.next
+    #     return count
         if head is None or head.next is None:
             return None
-        size=self.length(head)
-        brr=size//2
-        cur=head
-        for _ in range(brr-1):
-            cur=cur.next
-        cur.next=cur.next.next
+        fast,slow=head,head
+        prev=None
+        while fast is not None and fast.next is not None:
+            prev=slow
+            slow=slow.next
+            fast=fast.next.next
+        prev.next=slow.next
         return head
-
-    def length(self,head):
-        cur=head
-        count=0
-        while cur is not None:
-            count+=1
-            cur=cur.next
-        return count
