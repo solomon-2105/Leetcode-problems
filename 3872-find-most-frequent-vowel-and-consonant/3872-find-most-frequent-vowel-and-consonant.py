bac9962bb3280f6ee88a2e42742357d4ad7b02c8
Solomon <solomon.prem.sinai@gmail.com>
2025-09-13 09:07:23 +0530

    Time: 3 ms (65.94%), Space: 17.6 MB (89.16%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -1,11 +1,26 @@
+# class Solution:
+#     def maxFreqSum(self, s: str) -> int:
+#         a={}
+#         b={}
+#         c={'a','e','i','o','u'}
+#         for i in s:
+#             if i in c:
+#                 a[i]=a.get(i,0)+1
+#             else:
+#                 b[i]=b.get(i,0)+1
+#         return (max(a.values()) if a else 0)+(max(b.values()) if b else 0)
 class Solution:
     def maxFreqSum(self, s: str) -> int:
+        def is_vowel(char):
+            return char in 'aeiou'
         a={}
-        b={}
-        c={'a','e','i','o','u'}
         for i in s:
-            if i in c:
-                a[i]=a.get(i,0)+1
+            if i in a: a[i]+=1
+            else: a[i]=1
+        maxv,maxc=0,0
+        for k,v in a.items():
+            if is_vowel(k):
+                maxv=max(maxv,v)
             else:
-                b[i]=b.get(i,0)+1
-        return (max(a.values()) if a else 0)+(max(b.values()) if b else 0)
+                maxc=max(maxc,v)
+        return maxv+maxc
