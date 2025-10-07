f2483724dddbd4644941849e7781a93c1f8aff27
Solomon <solomon.prem.sinai@gmail.com>
2025-10-07 10:38:32 +0530

    Time: 0 ms (100%), Space: 18.5 MB (82.47%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -2,6 +2,7 @@ class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         fmax,smax,tmax=float('-inf'),float('-inf'),float('-inf')
         for i in nums:
+            if i==fmax or i==tmax or i==smax: continue
             if i>fmax:
                 tmax=smax
                 smax=fmax
@@ -9,6 +10,6 @@ class Solution:
             elif i>smax and i<fmax:
                 tmax=smax
                 smax=i
-            else:
+            elif i>tmax:
                 tmax=i
-        return fmax if tmax==float('-inf') else tmax
+        return tmax if tmax!=float('-inf') else fmax
