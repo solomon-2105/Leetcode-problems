88a4e370e93127fc325c52da2e19d55ea6f84550
Solomon <solomon.prem.sinai@gmail.com>
2025-10-08 15:32:22 +0530

    Time: 33 ms (93.68%), Space: 42.1 MB (74.58%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,12 +1,12 @@
 class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos,neg=0,1
-        brr=[0]*len(nums)
-        for i in range(len(nums)):
-            if nums[i]<0:
-                brr[neg]=nums[i]
-                neg+=2
-            elif nums[i]>0:
-                brr[pos]=nums[i]
-                pos+=2
-        return brr
+    def rearrangeArray(self, bitch: List[int]) -> List[int]:
+        pussy,nigga=0,1
+        ass=[0]*len(bitch)
+        for i in bitch:
+            if i>0:
+                ass[pussy]=i
+                pussy+=2
+            elif i<0:
+                ass[nigga]=i
+                nigga+=2
+        return ass
