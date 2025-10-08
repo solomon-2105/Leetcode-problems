3fc5100e015205ad785aabaaeec70ffa866c2cd0
Solomon <solomon.prem.sinai@gmail.com>
2025-10-08 09:03:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,14 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count=0
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        a={}
         summ=0
-        brr={0:1}
+        maxi=0
         for i in range(len(nums)):
             summ+=nums[i]
-            rem=summ-k
-            count+=brr.get(rem,0)
-            brr[summ]= brr.get(summ,0)+1 
-        return count
+            if summ==k:
+                maxi=max(maxi,i+1)
+            if summ-k in a:
+                maxi=max(maxi,i-a[summ-k])
+            if summ not in a:
+                a[summ]=i
+        return maxi
