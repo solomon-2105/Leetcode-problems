beccadbc1d36af4debd88536b21973ac4224a187
Solomon <solomon.prem.sinai@gmail.com>
2025-11-01 21:56:18 +0530

    Time: 3 ms (55.65%), Space: 17.7 MB (57.22%) - LeetHub

--- 3893-generate-tag-for-video-caption/3893-generate-tag-for-video-caption.py
+++ 3893-generate-tag-for-video-caption/3893-generate-tag-for-video-caption.py
@@ -2,18 +2,20 @@ class Solution:
     def generateTag(self, c: str) -> str:
         res='#'
         j=0
-        while j<len(c) and c[j]==" ":
+        while j<len(c) and c[j]==' ':
             j+=1
-        c=c[j:]
-        for i in range(len(c)):
+        i=j
+        while i<len(c):
             if len(res)>=100:
                 return res
             if c[i]==' ':
+                i+=1
                 continue
-            if i==0:
+            if i==j:
                 res+=c[i].lower()
             elif c[i-1]==' ':
                 res+=c[i].upper()
             else:
                 res+=c[i].lower()
+            i+=1
         return res
