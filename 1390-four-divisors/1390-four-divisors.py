d2edfeaa69c88e13ae14426a7d2eb35509e6c549
Solomon <solomon.prem.sinai@gmail.com>
2026-01-04 19:38:31 +0530

    Time: 177 ms (70.38%), Space: 18.6 MB (96.52%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.py
+++ 1390-four-divisors/1390-four-divisors.py
@@ -5,12 +5,13 @@ class Solution:
         for i in range( len(nums) ) :
             b.clear()
             c = self.divisors( nums[i] ,b)
-            if len(c) == 4 :
+            if c is not None and len(c) == 4 :
                 summ += sum(c)
         return summ
 
     def divisors(self, a, b):
         for i in range( 1, int( a ** 0.5 ) + 1 ) :
+            if len(b) > 4 : return None
             if a % i == 0 :
                 b.append(i)
                 if ( a // i ) != i :
