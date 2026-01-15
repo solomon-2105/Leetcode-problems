012bd227f1575a25996d0c45587d06b52590e4b3
Solomon <solomon.prem.sinai@gmail.com>
2026-01-15 10:45:19 +0530

    Time: 11 ms (31.46%), Space: 22.2 MB (39.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,29 +1,25 @@
-import math
 class Solution:
-    def majorityElement(self, a: List[int]) -> List[int]:
-        c1,c2,el1,el2,n=0,0,None,None,len(a)
-        for i in a:
-            if c1==0 and i!=el2:
-                c1=1
-                el1=i
-            elif c2==0 and i!=el1:
-                c2=1
-                el2=i
-            elif i==el1:
-                c1+=1
-            elif i==el2:
-                c2+=1
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c1 , c2 , el1 , el2 = 0 , 0 , None , None
+        for i in nums:
+            if c1 == 0 and i != el2:
+                c1 = 1
+                el1 = i
+            elif c2 == 0 and i != el1:
+                c2 = 1
+                el2 = i
+            elif i == el1:
+                c1 += 1
+            elif i == el2:
+                c2 += 1
             else:
-                c1-=1
-                c2-=1
-        brr,grr=0,0
-        for i in a:
-            if i==el1:brr+=1
-            elif i==el2:grr+=1
-        suiii=[]
-        if brr>math.floor(n/3):
-            suiii.append(el1)   
-        if grr>math.floor(n/3):
-            suiii.append(el2)
-        return suiii
-        
+                c1 -= 1
+                c2 -= 1
+        a , b = 0 , 0
+        for i in nums:
+            if i == el1 : a += 1
+            elif i == el2 : b += 1
+        temp = []
+        if a > len(nums)//3 : temp.append(el1)
+        if b > len(nums)//3 : temp.append(el2)
+        return temp
