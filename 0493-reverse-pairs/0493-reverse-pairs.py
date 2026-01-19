88c19bb8167c9565649f8e0082e8c17363262b8d
Solomon <solomon.prem.sinai@gmail.com>
2026-01-20 00:16:03 +0530

    Time: 1038 ms (12.73%), Space: 26.3 MB (46.94%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,47 +1,43 @@
 class Solution:
-    def merge(self,a,low,mid,high):
-        m=mid-low+1
-        n=high-mid
-        brr=[0]*m
-        grr=[0]*n
-        for i in range(m):
-            brr[i]=a[low+i]
-        for j in range(n):
-            grr[j]=a[mid+j+1]
-        i,j,huh=0,0,low
-        while i<m and j<n:
-            if brr[i]<=grr[j]:
-                a[huh]=brr[i]
-                i+=1
-            else:
-                a[huh]=grr[j]
-                j+=1
-            huh+=1
-        while i<m:
-            a[huh]=brr[i]
-            i+=1
-            huh+=1
-        while j<n:
-            a[huh]=grr[j]
-            j+=1
-            huh+=1
-
-    def count_pairs(self,a,low,mid,high):
-        right=mid+1
-        for i in range(low,mid+1):
-            while right<=high and a[i]>2*a[right]:
-                right+=1
-            self.count=self.count+(right-(mid+1))
-
-    def merge_sort(self,a,low,high):
-        if low<high:
-            mid=low+(high-low)//2
-            self.merge_sort(a,low,mid)
-            self.merge_sort(a,mid+1,high)
-            self.count_pairs(a,low,mid,high)
-            self.merge(a,low,mid,high)
-
-    def reversePairs(self,nums):
-        self.count=0
-        self.merge_sort(nums,0,len(nums)-1)
+    def reversePairs(self, a: List[int] ) -> int:
+        self.count = 0
+        self.merge_sort(a, 0, len(a) - 1)
         return self.count
+
+    def merge_sort(self,a, l, r):
+        if l < r:
+            m = l + (r - l) // 2
+            self.merge_sort(a, l, m)
+            self.merge_sort(a, m + 1, r)
+            self.reverse_pair(a,l,m,r)
+            self.merge(a, l, m, r)
+
+    def merge(self,a, l, m, r):
+        temp = []
+        left = l
+        right = m + 1
+
+        while left <= m and right <= r:
+            if a[left] <= a[right]:
+                temp.append(a[left])
+                left += 1
+            else:
+                temp.append(a[right])
+                right += 1
+
+        while left <= m:
+            temp.append(a[left])
+            left += 1
+
+        while right <= r:
+            temp.append(a[right])
+            right += 1
+
+        for i in range(len(temp)):
+            a[l + i] = temp[i]
+
+    def reverse_pair(self,a,l,m,h):
+        r = m + 1
+        for i in range(l,m+1):
+            while r <= h and a[i] > 2 * a[r] : r += 1
+            self.count += (r - (m+1))
