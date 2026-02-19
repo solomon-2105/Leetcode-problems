439b87675145c77d53cd523f6a4bbb0b8e03e47d
Solomon <solomon.prem.sinai@gmail.com>
2026-02-19 15:16:09 +0530

    Time: 43 ms (70.2%), Space: 28.6 MB (71.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,13 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        mini=prices[0] 
-        profit=0
-        for i in prices:
-            if i<mini:
-                mini=i
-            if profit<(i-mini):
-                profit=i-mini
+    def maxProfit(self, p: List[int]) -> int:
+        # 7 , 1 , 5 , 3 , 6 , 4
+        # whenever u come across a better deal, 
+        # that is when the price u brought at x day is higher than the current price , we buy in the current day , and look for higher prices in the future days
+        profit = 0
+        m = p[0]
+        for i in range(1 , len(p)):
+            if m < p[i] :
+                profit = max ( profit , p[i] - m)
+            else:
+                m = p[i]
         return profit
