a9b66915a36d355224676be7213b587b6730a2f3
Solomon <solomon.prem.sinai@gmail.com>
2026-02-19 09:57:41 +0530

    Time: 7 ms (57.64%), Space: 21.3 MB (37.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,27 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        el=None
-        count=0
-        i=0
-        while i<len(nums):
-            if count==0:
-                el=nums[i]
-            if nums[i]==el:
-                count+=1
+        # If we pair one majority element with one different element,
+        # they cancel each other.
+        # Since majority element appears more than n/2 times,
+        # it will still remain at the end.
+        # 2 , 2 , 1 , 1 , 1 , 2 , 2 
+        # here 2 is majority element as we can see , 
+        # each element cancels out with 2 and 2 still remains at the end
+        #thus it's the majority element
+
+        count = 0
+        el = None
+        for i in nums:
+            # If count becomes 0, choose current number as new candidate.
+            if count == 0:
+                el = i
+            
+            # If current number is same as candidate, increase count.
+            # Otherwise decrease count (cancel out).
+            if i == el:
+                count += 1
             else:
-                count-=1
-            i+=1
-        # b=0
-        # for i in nums:
-        #     if i==el: b+=1
-        # return el if b>len(nums)//2 else -1
+                count -= 1
+        
+        # The remaining candidate must be the majority element.
         return el
