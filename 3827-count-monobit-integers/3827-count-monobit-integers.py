9402312714d5f3edcd171d846171ed15c1933176
Solomon <solomon.prem.sinai@gmail.com>
2026-02-22 00:48:40 +0530

    Time: 231 ms (5.02%), Space: 19.5 MB (28.41%) - LeetHub

---
+++ 3827-count-monobit-integers/3827-count-monobit-integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMonobit(self, n: int) -> int:
+        if n == 0 : return 1
+        if n == 1: return 2
+        count = 2
+        for i in range(3,n+1):
+            a = [0,0]
+            while i:
+                if i % 2 == 0: a[0] = 1
+                else : a[1] = 1
+                i //= 2
+            if a[0] != a[1]:
+                count += 1
+        return count
