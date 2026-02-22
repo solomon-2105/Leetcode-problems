dbd7101f358901b923a93c486b97a6fbad638a60
Solomon <solomon.prem.sinai@gmail.com>
2026-02-23 01:00:52 +0530

    Time: 29 ms (79.56%), Space: 21.9 MB (70.17%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        a={0:1}
-        presum=0
-        count=0
+        a = {0:1}
+        presum = 0
+        count = 0
         for i in nums:
-            presum+=i
+            presum += i
 
-            if presum-k in a:
-                count+=a[presum-k]
+            if presum - k in a:
+                count += a[presum-k]
             
-            a[presum]=a.get(presum,0)+1
+            a[presum] = a.get(presum,0)+1
         return count
             
