c3b196f7d6afd9777f194ccf5e020bd34a5f950e
Solomon <solomon.prem.sinai@gmail.com>
2026-02-23 01:04:40 +0530

    Time: 2 ms (94.89%), Space: 19.8 MB (59.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxProduct(self, a: List[int]) -> int:
-        maxi=-sys.maxsize
-        prefix=suffix=1
+        maxi = float('-inf')
+        prefix = suffix = 1
         for i in range(len(a)):
-            prefix*=a[i]
-            maxi=max(maxi,prefix)
-            if a[i]==0:
-                maxi=max(0,maxi)
-                prefix=1
+            prefix *= a[i]
+            maxi = max(maxi,prefix)
+            if a[i] == 0:
+                maxi = max(0,maxi)
+                prefix = 1
         for i in range(len(a)-1,-1,-1):
-            suffix*=a[i]
-            maxi=max(maxi,suffix)
-            if a[i]==0:
-                maxi=max(0,maxi)
-                suffix=1
+            suffix *= a[i]
+            maxi = max(maxi,suffix)
+            if a[i] == 0:
+                maxi = max(0,maxi)
+                suffix = 1
         return maxi
