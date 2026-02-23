0ff5866dcfe134d22e1a51d8095e9e91162b1b28
Solomon <solomon.prem.sinai@gmail.com>
2026-02-23 21:44:33 +0530

    Time: 18 ms (71.19%), Space: 51.5 MB (66.62%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.py
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -1,9 +1,12 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         n = len(customers)
+        total_waiting_time = 0
         current_time = 0
-        waiting_time = 0
-        for i in range(n):
-            current_time = max( current_time , customers[i][0]) + customers[i][1]
-            waiting_time += current_time - customers[i][0]
-        return waiting_time / n
+        for arrival_time , cooking_time in customers:
+            if current_time < arrival_time :
+                current_time = arrival_time
+            waiting_time = current_time + cooking_time - arrival_time
+            total_waiting_time += waiting_time
+            current_time += cooking_time
+        return total_waiting_time / n
