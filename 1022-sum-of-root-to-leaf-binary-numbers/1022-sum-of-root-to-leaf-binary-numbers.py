d1b849422d563194e7d0a526a73f0a762b3eb4e6
Solomon <solomon.prem.sinai@gmail.com>
2026-02-24 19:33:55 +0530

    Time: 0 ms (100%), Space: 19.6 MB (25.88%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -5,25 +5,36 @@
 #         self.left = left
 #         self.right = right
 
+# class Solution:
+#     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+#         paths = self.collect_paths(root)
+#         total = 0
+#         for p in paths:
+#             total += int(p, 2)  
+#         return total
+
+#     def collect_paths(self, root):
+#         res = []
+#         def dfs(node, path):
+#             if not node:
+#                 return
+#             path.append(str(node.val))
+#             if not node.left and not node.right:
+#                 res.append("".join(path))
+#             else:
+#                 dfs(node.left, path)
+#                 dfs(node.right, path)
+#             path.pop()
+#         dfs(root, [])
+#         return res
+
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        paths = self.collect_paths(root)
-        total = 0
-        for p in paths:
-            total += int(p, 2)  
-        return total
-
-    def collect_paths(self, root):
-        res = []
-        def dfs(node, path):
+        def dfs(node, current):
             if not node:
-                return
-            path.append(str(node.val))
+                return 0
+            current = current * 2 + node.val
             if not node.left and not node.right:
-                res.append("".join(path))
-            else:
-                dfs(node.left, path)
-                dfs(node.right, path)
-            path.pop()
-        dfs(root, [])
-        return res
+                return current
+            return dfs(node.left, current) + dfs(node.right, current)
+        return dfs(root, 0)
