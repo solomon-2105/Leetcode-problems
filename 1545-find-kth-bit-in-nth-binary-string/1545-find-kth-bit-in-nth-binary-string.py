e4e637425753d59c4afa5589730496f709980ccc
Solomon <solomon.prem.sinai@gmail.com>
2026-03-03 11:42:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (89.71%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,19 +1,39 @@
+import math
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        def findit(n):
-            if n == 1:
-                return "0"
-            n1 = findit(n-1)
-            return n1 + "1" + reverse(invert(n1))
         
-        def reverse(a):
-            return a[::-1]
+        #BRUTEFORCE APPROACH (SIMULATION OF RECURSION / BUILDING THE STRING FROM SCRATCH BITCHES)
+        # def findit(n):
+        #     if n == 1:
+        #         return "0"
+        #     n1 = findit(n-1)
+        #     return n1 + "1" + reverse(invert(n1))
+        
+        # def reverse(a):
+        #     return a[::-1]
 
-        def invert(a):
-            res = ""
-            for i in a:
-                res += str(1 - int(i))
-            return res
+        # def invert(a):
+        #     res = ""
+        #     for i in a:
+        #         res += str(1 - int(i))
+        #     return res
 
-        string = findit(n)
-        return string[k-1]
+        # string = findit(n)
+        # return string[k-1]
+
+#===============================================================================
+        
+        #OPTIMAL APPROACH EASY AS FUCK
+
+        if n == 1:
+            return "0"
+
+        length = (1 << n) - 1
+        mid = (length // 2) + 1
+        if k < mid:
+            return self.findKthBit(n - 1, k)
+        elif k == mid:
+            return "1"
+        else:
+            ch = self.findKthBit(n - 1, length - k + 1)
+            return "1" if ch == "0" else "0"
